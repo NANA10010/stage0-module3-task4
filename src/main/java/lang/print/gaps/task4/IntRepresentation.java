da6695ede09f59228437c26f193f30f7c5775842
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-01-28 21:20:49 +0400

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+         System.out.print('\u0063');
+            System.out.print('\u0068');
+            System.out.print('\u0061');
+            System.out.print('\u0072');
     }
 }
